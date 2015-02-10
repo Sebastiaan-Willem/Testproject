275cbc70e9e447cbd9ba77706ace47bfef98dac5
Silke Venneman <silke.venneman.t3309@student.hogent.be>
2015-02-10 11:35:45 +0100

    optel programma

--- src/Testproject.java
+++ src/Testproject.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,11 +11,28 @@
  * @author Silke
  */
 public class Testproject {
-    public Testproject() {
+    private static int getal;
+    
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+                
+        System.out.print("Geef een geheel getal in: ");
+        int getal = sc.nextInt();
         
+        System.out.print("Hoeveel wil je erbij optellen? ");
+        int optelGetal = sc.nextInt();
+        
+        int som = telOp(getal, optelGetal);
+        
+        System.out.printf("%d + %d = %d%n", getal, optelGetal, som);
     }
     
     public void print() {
         System.out.println("Test Test");
+    }
+    
+    public static int telOp(int getal, int nummer) {
+        int som = getal + nummer;
+        return som;
     }
 }
